2fdd192a19756ee8fd856e0289f21b965b3adfc3
rrsin <oo8923@boinit.com>
2025-12-24 16:33:11 +0900

    Adjust top search area right margin to avoid MapLibre controls overlap

--- src/features/map/MapPage.tsx
+++ src/features/map/MapPage.tsx
@@ -197,13 +197,13 @@ export function MapPage() {
       zoom: 11,
     });
 
-    map.addControl(new maplibregl.NavigationControl(), "bottom-right");
+    map.addControl(new maplibregl.NavigationControl(), "top-right");
     map.addControl(
       new maplibregl.GeolocateControl({
         positionOptions: { enableHighAccuracy: true },
         trackUserLocation: false,
       }),
-      "bottom-right"
+      "top-right"
     );
 
     mapRef.current = map;
@@ -525,7 +525,7 @@ export function MapPage() {
   return (
     <div className="h-dvh w-full relative">
       {/* 상단: 검색 + 설정 */}
-      <div className="absolute z-20 left-3 right-3 top-3 space-y-2">
+      <div className="absolute z-20 left-3 right-16 top-3 space-y-2">
         <NominatimSearch
           value={q}
           onChange={setQ}
